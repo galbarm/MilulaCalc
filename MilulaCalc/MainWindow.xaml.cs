3790c19a14b914010988b61944c1ce79ef14098a
Gal Bar Mashiah <galbarm@gmail.com>
2015-08-16 23:06:36 +0300

    First Commit

---
+++ MilulaCalc/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Expression = NCalc.Expression;
+
+
+namespace MilulaCalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            _ExpressionBox.Focus();
+
+            new HotKey(Key.Space, KeyModifier.Alt, OnHotKeyHandler);
+        }
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                var expression = new Expression(_ExpressionBox.Text);
+                _ResultBox.Text = expression.Evaluate().ToString();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Clipboard.SetText(_ResultBox.Text);
+            }
+        }
+
+        private void OnHotKeyHandler(HotKey hotKey)
+        {
+            WindowState = WindowState.Minimized;
+            Show();
+            WindowState = WindowState.Normal;
+        }
+
+        private void OnStateChanged(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
